6567a84c72d37af9edfbf337919f00f547a12b19
Kenny Ek <ekkenny@hotmail.com>
2020-07-02 11:44:26 +0200

    Update logging for testing

--- index.js
+++ index.js
@@ -2,8 +2,22 @@ const { Toolkit } = require('actions-toolkit');
 
 Toolkit.run(async tools => {
   const pkg = tools.getPackageJSON();
-  tools.log('Will log tools.context');
-  tools.log(tools.context);
+  const ref = tools.context.payload.ref;
+  const version = ref.split('/').pop();
+  const [major, minor, patch] = version.split('.');
+  const [pkgMajor, pkgMinor, pkgPatch] = pkg.version.split('.');
+
+  tools.log({
+    ref,
+    version,
+    pkgVersion: pkg.version,
+    major,
+    minor,
+    patch,
+    pkgMajor,
+    pkgMinor,
+    pkgPatch
+  });
 }, {
   event: 'create'
 });
