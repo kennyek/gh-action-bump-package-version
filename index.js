a817453e109790406f2cbabe6a5ded289935ef0f
Kenny Ek <ekkenny@hotmail.com>
2020-07-02 10:52:19 +0200

    Add log for testing

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ const { Toolkit } = require('actions-toolkit');
 
 Toolkit.run(async tools => {
   const pkg = tools.getPackageJSON();
+  tools.log(JSON.stringify(tools.context.event, null, 2));
+}, {
+  event: 'create'
 });
